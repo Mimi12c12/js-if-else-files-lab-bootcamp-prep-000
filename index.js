301bbf14c2e22dc007c6acc7a0e0f32030440b1d
Misha Chapman <mishachapman14@hotmail.com>
2019-04-16 13:33:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ var name = "Joe"
 var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
-}
+} else {
+  console.log("Expected the name to Susan")
 
 // Don't worry about this
 module.exports = { name, height
-} else {
-  console.log("Expected the name to Susan")
-}
 
